a65fd00ed73c2bb2a40964263ee3128a147d2c0c
omrajsharma <omraj7413@gmail.com>
2023-09-22 21:18:42 +0530

    first commit 🚀

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Hero from "./pages/Hero";
+import MainLayout from "./layout/MainLayout";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />} >
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/hero/:heroId" element={<Hero />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
