644342837a3b7a881c716bd3aafc7aa4be1445f2
omrajsharma <omraj7413@gmail.com>
2023-10-03 21:10:21 +0530

    feat: register user

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  direction: 'rtl',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
