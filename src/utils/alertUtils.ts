2477c2e9294ec35c0b49b34a515295c5534132b2
omrajsharma <omraj7413@gmail.com>
2023-10-14 07:16:15 +0530

    feat: alert integration in apiUtils

--- src/utils/alertUtils.ts
+++ src/utils/alertUtils.ts
@@ -1,6 +1,6 @@
 import { enqueueSnackbar } from "notistack";
 
-enum AlertType {
+export enum AlertTypeEnum {
   SUCCESS,
   ERROR,
   WARNING,
@@ -8,23 +8,23 @@ enum AlertType {
   DEFAULT,
 }
 
-export default function AppAlert(alertType: AlertType, message: string) {
-  if (alertType === AlertType.SUCCESS) {
+export default function AppAlert(alertType: AlertTypeEnum, message: string) {
+  if (alertType === AlertTypeEnum.SUCCESS) {
     enqueueSnackbar(message, {
       variant: "success",
       anchorOrigin: { vertical: "top", horizontal: "right" },
     });
-  } else if (alertType === AlertType.ERROR) {
+  } else if (alertType === AlertTypeEnum.ERROR) {
     enqueueSnackbar(message, {
       variant: "error",
       anchorOrigin: { vertical: "top", horizontal: "right" },
     });
-  } else if (alertType === AlertType.WARNING) {
+  } else if (alertType === AlertTypeEnum.WARNING) {
     enqueueSnackbar(message, {
       variant: "warning",
       anchorOrigin: { vertical: "top", horizontal: "right" },
     });
-  } else if (alertType === AlertType.INFO) {
+  } else if (alertType === AlertTypeEnum.INFO) {
     enqueueSnackbar(message, {
       variant: "info",
       anchorOrigin: { vertical: "top", horizontal: "right" },
--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,5 +1,7 @@
 import API_ENUM from "../enum/API_ENUM"
 import ApiEnumToParams from "../converter/ApiEnumToParams";
+import AppAlert from "./alertUtils";
+import { AlertTypeEnum } from "./alertUtils";
 
 const apiCall = async (
     API_ENUM: API_ENUM,
@@ -9,11 +11,25 @@ const apiCall = async (
 
     try {
         if (params?.url) {
-            fetch(params?.url, {
+            const response = await fetch(params?.url, {
                 method: params.method,
                 headers: params.header,
                 body: JSON.stringify(body),
             })
+
+            if (response.ok) {
+                const data = await response.json();
+
+                if ( data?.message.length > 0 )
+                    AppAlert(AlertTypeEnum.SUCCESS, data?.message);
+                
+                return data.data;
+            } else {
+                const data = await response.json();
+
+                if ( data?.error.length > 0 )
+                    AppAlert(AlertTypeEnum.ERROR, data?.error);
+            }
         }
     } catch (err) {
         // logging
